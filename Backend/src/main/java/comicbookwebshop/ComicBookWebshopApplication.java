97316acb947aaf4144db53ffe218eff2112b2630
Barna Udvarhelyi <barna.udvarhelyi@gmail.com>
2023-06-06 11:04:16 +0200

    Project and File sys Created

---
+++ Backend/src/main/java/comicbookwebshop/ComicBookWebshopApplication.java
@@ -0,0 +1,13 @@
+package comicbookwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicBookWebshopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ComicBookWebshopApplication.class, args);
+  }
+
+}
---
+++ Backend/src/test/java/comicbookwebshop/ComicBookWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package comicbookwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicBookWebshopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
